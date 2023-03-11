a6f05caebae866dfbeaf1d15a1095dfab83dfff4
carlangas <carlos.hernandezmar@anahuac.mx>
2023-03-11 12:01:39 -0600

    se redujeron algunos widgets en nuevas clases

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'descripcion_lugar.dart';
-import 'reviews_info.dart';
+import 'lista_reviewers.dart';
 import 'encabezado_appbar.dart';
 
 void main() {
@@ -19,19 +19,9 @@ class MyApp extends StatelessWidget {
             Stack (
               children: [
                 ListView(
-
                   children: [
                     Descripcion_lugar(),
-                    reviews_info("assets/img/people.jpg", "Carlos", "1 review · 5 photos", "Wooooooow"),
-                    reviews_info("assets/img/girl.jpg", "Girl", "1 review · 5 photos", "Que bonito"),
-                    reviews_info("assets/img/ann.jpg", "user03", "1 review · 5 photos", "Que bonito"),
-                    reviews_info("assets/img/beach_palm.jpeg", "otro", "1 review · 5 photos", "Que bonito"),
-                    reviews_info("assets/img/beach_palm.jpeg", "otro", "1 review · 5 photos", "Que bonito"),
-                    reviews_info("assets/img/beach_palm.jpeg", "otro", "1 review · 5 photos", "Que bonito"),
-                    reviews_info("assets/img/beach_palm.jpeg", "otro", "1 review · 5 photos", "Que bonito"),
-                    reviews_info("assets/img/beach_palm.jpeg", "otro", "1 review · 5 photos", "Que bonito"),
-                    reviews_info("assets/img/beach_palm.jpeg", "otro", "1 review · 5 photos", "Que bonito"),
-                    reviews_info("assets/img/beach_palm.jpeg", "otro", "1 review · 5 photos", "Que bonito"),
+                    lista_reviewers()
                   ],
                 ),
                 Encabezado(),
