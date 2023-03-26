3186a4c81cb2257644d300756d072f68cb859d74
carlangas <carlos.hernandezmar@anahuac.mx>
2023-03-26 00:51:06 -0600

    botones en stateless

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'main_section.dart';
-import 'profile_info.dart';
-
 
 void main() {
   runApp(MyApp());
@@ -11,11 +8,143 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: profile_info()
-        //Esta es la seccion que habiamos trabajado, esta en main_section.dart
-        //muestra_main()
+      title: 'Mi aplicación',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
 
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mi aplicación'),
+      ),
+      body: Center(
+        child: Botones(),
+      ),
+    );
+  }
+}
+
+class Botones extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => InicioScreen()),
+            );
+          },
+          child: Container(
+            child: Icon(Icons.home),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            padding: EdgeInsets.all(16.0),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ProfileScreen()),
+            );
+          },
+          child: Container(
+            child: Icon(Icons.person),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            padding: EdgeInsets.all(16.0),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => FavoritosScreen()),
+            );
+          },
+          child: Container(
+            child: Icon(Icons.favorite),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            padding: EdgeInsets.all(16.0),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MyHomePage()),
+            );
+          },
+          child: Container(
+            child: Icon(Icons.accessible_forward),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            padding: EdgeInsets.all(16.0),
+          ),
+        ),
+      ],
+    );
+  }
+
+}
+
+class InicioScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio'),
+      ),
+      body: Center(
+        child: Botones(),
+      ),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Perfil'),
+      ),
+      body: Center(
+        child: Botones(),
+      ),
+    );
+  }
+}
+
+class FavoritosScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Favoritos'),
+      ),
+      body: Center(
+        child: Botones(),
       ),
     );
   }
