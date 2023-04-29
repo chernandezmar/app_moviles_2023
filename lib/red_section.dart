c3d2a7c84b13ff119f093ddada51db3219e3743c
carlangas <carlos.hernandezmar@anahuac.mx>
2023-04-29 09:21:34 -0600

    cambios en nueva seccion

--- lib/red_section.dart
+++ lib/red_section.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
 import 'menu.dart';
+import 'gradiente_fondo.dart';
 
 
 class muestra_red extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
+    final SeccionMenu = Container(
+        margin: const EdgeInsets.only(
+          top: 150,
+        ),
+      child: muestra_menu(),
+
+    );
+
+    final icono_fav = Center (
+        heightFactor: 1.4,
+        widthFactor: 4,
+      child: Container(
+        child: Icon(
+          Icons.favorite_border,
+          color: Colors.white,
+          size: 122,
+        )
+    )
+    );
+
+
+    final infoRegreso = Stack(
+      children: [
+        GradientBack("", 220),
+        icono_fav,
+        SeccionMenu,
+      ],
+    );
+
+
+
     return MaterialApp(
         home: Scaffold(
-            body: Container(
-              color: Colors.red,
-              height: 500,
-              width: 500,
-              child: muestra_menu(),
-            )
+            body: infoRegreso,
         )
     );
   }
